70307c9c4134d29812c2de434d0301cf2a85b816
numebegir <152449467+numebegir@users.noreply.github.com>
2024-02-28 17:52:27 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+    data = generate_random_data()
+
+import random
+if __name__ == "__main__":
+    main()
+
+def main():
+        print(f"Random Number: {item}")
+def generate_random_data():
+    for item in data:
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
