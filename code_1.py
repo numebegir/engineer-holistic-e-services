fa55c85bb35ffb11811b97802ab46edc2a1fa5a3
numebegir <152449467+numebegir@users.noreply.github.com>
2024-02-29 17:06:26 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,41 +1,30 @@
-    data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-    main()
-    data = generate_random_data()
 def generate_random_data():
-def main():
-
 
 
         print(f"Random Number: {item}")
+
+    return data
     for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
 if __name__ == "__main__":
-    return data    const data = [];
+    data = generate_random_data()
+    main()
+def main():
+import random}
     for (let i = 0; i < 10; i++) {
-function generateRandomData() {
 main();
-
-    return data;
-
-function main() {
+    const data = [];
+    const data = generateRandomData();
+    }
 }
         data.push(Math.floor(Math.random() * 100) + 1);
-    });
-    data.forEach(item => {
-    const data = generateRandomData();
         console.log("Random Number: " + item);
-}
-    }
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-}
 
-    }
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
-        }
-        int[] data = new int[10];
-        }
-        for (int i = 0; i < 10; i++) {
+    });
+function generateRandomData() {
+
+
+    data.forEach(item => {
+    return data;
+function main() {
